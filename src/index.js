42aa00bc2b681c5f101e6de670a808d4396bf0d9
clem8109 <clementroger81@gmail.com>
2021-08-17 23:08:03 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Test!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+var teest = function(){
+
+    return {
+        init: function(){
+          alert('test');
+          function test() {
+           
+          }sdsd
+        }sd
+    }
+}();dsdd
